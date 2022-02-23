05f4a2f0c01e4454b7ec4b0fb0b5d731f09dcf93
Amin Azari <35372093+AminAzari@users.noreply.github.com>
2022-02-23 16:41:45 +0100

    Update sol.m

--- sol.m
+++ sol.m
@@ -1,15 +1,19 @@
-for n=21
-    S=zeros(n,n);
-    S(1,:)=ones(1,n);
-    S(:,1)=1:n;
+clc
+clear all
+N=20 
+
+n=N+1;
+S=zeros(n,n);
+S(1,:)=ones(1,n);
+S(:,1)=1:n;
     
     
-    for i=2:n
-        for j=2:n
-            S(i,j)=S(i-1,j)+S(i,j-1);
-        end
-    end
-    
-    S(n,n-1)
+for i=2:n
+    for j=2:n
+        S(i,j)=S(i-1,j)+S(i,j-1);
+     end
 end
+    
+paths=S(n,N)
+ 
 
